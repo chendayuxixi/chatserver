7511cb42a265adbfe97d5c449764c78e1adcc5d5
chendayuxixi <840075893@qq.com>
2024-07-01 20:57:56 +0800

    第一次提交

---
+++ src/server/chatserver.cpp
@@ -0,0 +1,46 @@
+#include"chatserver.hpp"
+#include<functional>
+#include"json.hpp"
+#include"chatservice.hpp"
+using namespace std;
+using namespace placeholders;
+using json = nlohmann::json;
+
+
+ChatServer::ChatServer(EventLoop *loop,
+               const InetAddress &listenAddr,
+               const string &nameArg)
+    :_server(loop, listenAddr, nameArg), _loop(loop) {
+        //注册连接回调
+        _server.setConnectionCallback(std::bind(&ChatServer::onConnection, this, _1));
+        //注册消息回调
+        _server.setMessageCallback(std::bind(&ChatServer::onMessage, this, _1, _2, _3));
+        //设置线程数量
+        _server.setThreadNum(4);
+    }
+
+    void ChatServer::start() {
+        _server.start();
+    }
+    // 上报链接相关信息的回调函数
+    void ChatServer::onConnection(const TcpConnectionPtr & conn ) {
+        //用户断开连接
+        if(!conn->connected()) {
+            
+            ChatService::instance()->clientCloseException(conn);
+            conn->shutdown();
+        }
+
+    }
+
+    // 上报读写事件相关信息的回调函数
+    void ChatServer::onMessage(const TcpConnectionPtr &conn, Buffer * buffer, Timestamp time) {
+        string buf = buffer->retrieveAllAsString();
+        //数据解码
+        json js  = json::parse(buf);
+        //通过js["msgid"]获取业务
+
+        auto msgHandler = ChatService::instance()->getHandler(js["msgid"].get<int>());
+        msgHandler(conn, js, time);
+ 
+    }
